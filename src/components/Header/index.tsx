978cac826857fde630a0c4b8055486bb5d1c0f05
Jackson Nascimento <jsnascimento1@hotmail.com>
2022-03-29 18:35:36 -0300

    Cria modal de cadastro de nova transação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
+import { useState } from 'react'
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
+import Modal from 'react-modal'
 
-export function Header(){
+interface HeaderProps{
+  onOpenNewTransactionModal: () => void; //prop que é uma função cujo retorno é  vazio
+}
+
+export function Header({onOpenNewTransactionModal}: HeaderProps){
   return(
     <Container>
       <Content>
       <img src= {logoImg} alt="DT Money" />
-      <button type="button">Nova transação</button>
+      <button type="button" onClick={onOpenNewTransactionModal}>Nova transação</button>
+ 
       </Content>
     </Container>
   )
