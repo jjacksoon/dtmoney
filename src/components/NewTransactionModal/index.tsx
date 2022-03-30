a8e81e3582a7e2c648e02bce717fbc39d2054730
Jackson Nascimento <jsnascimento1@hotmail.com>
2022-03-30 16:33:32 -0300

    Cria e estiliza botões income e outcome

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,8 @@
 import Modal from 'react-modal'
-import { Container } from './styles';
+import { Container, TransactionTypeContainer } from './styles';
 import closeImg from '../../assets/close.svg'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
 
 interface NewTransactionModalProps{
   isOpen: boolean;
@@ -24,6 +26,21 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
         <h2>Cadastrar Transação</h2>
         <input placeholder="Título" />
         <input type="number" placeholder="Valor" />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada"/>
+            <span>Entrada</span>
+          </button>
+
+          <button type="button">
+            <img src={outcomeImg} alt="Saída"/>
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
+
+
         <input placeholder="Categoria" />
         <button type="submit">Cadastrar</button>
       </Container>
