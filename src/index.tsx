da7156cd5eab37d4172aeb45d6a313fe83c9b09b
Jackson Nascimento <jsnascimento1@hotmail.com>
2022-04-02 10:59:20 -0300

    Insere dados da transação na API e formata valores de moeda e data

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,31 @@ import {App} from './App';
 import { createServer, Model } from 'miragejs';
 
 createServer({
-  models: {
-    transaction: Model, 
+  models: {   //Criação de um BD da API
+    transaction: Model, //BD Vazio
+  },
+
+  seeds(server){
+    server.db.loadData({
+      transactions: [
+        {
+        id:1, 
+        title: 'Freelance de website', 
+        type: 'deposit', 
+        category: 'Development', 
+        amount: 6000, 
+        createdAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id:2, 
+          title: 'Aluguel', 
+          type: 'withdraw', 
+          category: 'Casa', 
+          amount: 1200, 
+          createdAt: new Date('2021-02-14 11:00:00')
+        },
+      ],
+    })
   },
 
   routes(){
@@ -18,7 +41,7 @@ createServer({
 
     //Requisição POST
     this.post('/transactions', (schema, request) =>{
-      const data = JSON.parse(request.requestBody)
+      const data = JSON.parse(request.requestBody) //conversão para JSON
       return schema.create('transaction', data); //schema = banco de dados
     })
   }
