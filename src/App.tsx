4c01f6f7ab4a0a7a6597afab8527c9b0f9349f1a
Jackson Nascimento <jsnascimento1@hotmail.com>
2022-04-03 08:50:11 -0300

    Cria o Contexto para compartilhar informações entre diversos componentes da minha aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { GlobalStyle } from "./styles/global";
 import Modal from 'react-modal'
 import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
+import {TransactionsProvider } from "./TransactionsContext";
 
 Modal.setAppElement('#root') //Acessibildade
 
@@ -18,7 +19,7 @@ export function App() {
     setIsNewTransactionModalOpen(false);
   }
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
 
       <NewTransactionModal
@@ -29,6 +30,6 @@ export function App() {
       <Dashboard/>
 
       <GlobalStyle/>
-    </>
+    </TransactionsProvider>
   );
 }
---
+++ src/TransactionsContext.tsx
@@ -0,0 +1,45 @@
+import  {createContext, ReactNode, useEffect, useState} from 'react'
+import { api } from './services/api';
+
+interface Transaction{
+  id: number, 
+  title: string, 
+  amount: number, 
+  type: string, 
+  category:string, 
+  createdAt: string
+}
+interface TransactionsProviderProps{
+  children: ReactNode; //Tipagem ReactNode aceita qualquer tipo de conteudo valido pelo react (tags html, jsx, texto ou qualquer coisa que eu possa colocar dentro do jsx)
+}
+
+//Fazendo, via typescript, que o TransactionIput herde da interface Transaction todas as tipagens, exceto id e createdAt
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>;
+
+interface TransactionsContextData {
+  transactions: Transaction[],
+  createTransaction: (transaction: TransactionInput) => void
+}
+
+export const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData);
+
+export function TransactionsProvider({children}: TransactionsProviderProps){
+  const[transactions, setTransactions] = useState<Transaction[]>([]);
+  
+  //Requisição para a API Fictícia
+  useEffect(() => {
+    api.get('transactions')
+    .then(response => setTransactions(response.data.transactions))
+  }, []);
+  
+  function createTransaction(transaction: TransactionInput){
+    api.post('/transactions', transaction)
+  }
+
+
+  return(
+    <TransactionsContext.Provider value={{transactions, createTransaction}}>
+      {children}
+    </TransactionsContext.Provider>
+  );
+}
